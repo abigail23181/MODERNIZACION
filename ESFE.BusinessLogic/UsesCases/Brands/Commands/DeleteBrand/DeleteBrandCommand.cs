bf162ad9f259c3f47be501ef34280ee5970b53ec
abigail23181 <abigailrodrigxz@gmail.com>
2026-05-13 15:04:23 -0600

    Abigail envio cambio de carpeta delete

---
+++ ESFE.BusinessLogic/UsesCases/Brands/Commands/DeleteBrand/DeleteBrandCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFE.BusinessLogic.UsesCases.Brands.Commands.DeleteBrand
+{
+    public record DeleteBrandCommand(int brandId) : IRequest<int>
+    {
+    }
+}
---
+++ ESFE.BusinessLogic/UsesCases/Brands/Commands/DeleteBrand/DeleteBrandHandler.cs
@@ -0,0 +1,26 @@
+﻿using ESFE.DataAccess.Interfaces;
+using ESFE.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE.BusinessLogic.UsesCases.Brands.Commands.DeleteBrand;
+
+internal sealed class DeleteBrandHandler(IEfRepository<Brand> _repository)
+    : IRequestHandler<DeleteBrandCommand, int>
+{
+    public async Task<int> Handle(DeleteBrandCommand command, CancellationToken cancellationToken)
+    {
+
+        var existingBrand = await _repository.GetByIdAsync(command.brandId);
+
+        if (existingBrand is null) return 0;
+
+        await _repository.DeleteAsync(existingBrand, CancellationToken);
+
+        return existingBrand.BrandId;
+    }
+}
