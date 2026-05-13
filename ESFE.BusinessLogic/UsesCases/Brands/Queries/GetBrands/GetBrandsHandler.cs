8a60635818e56a98f2a953bd1afdd15b6ee79ee5
abigail23181 <abigailrodrigxz@gmail.com>
2026-05-13 15:41:07 -0600

    Se enviaron cambios

---
+++ ESFE.BusinessLogic/UsesCases/Brands/Queries/GetBrands/GetBrandsHandler.cs
@@ -0,0 +1,27 @@
+﻿using ESFE.BusinessLogic.DTOs;
+using ESFE.DataAccess.Interfaces;
+using ESFE.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFE.BusinessLogic.UsesCases.Brands.Queries.GetBrands
+{
+    internal sealed class GetBrandsHandler(IEfRepository<Brand> _repository)
+        : IRequestHandler<GetBrandsQuery, <List<BrandResponse>
+    {
+{
+        public async Task<object> Handle(GetBrandsQuery query, CancellationToken cancellationToken)
+        {
+            var brands = await _repository.ListAsync(cancellationToken);
+
+            if (brands == null || !brands.Any())
+            {
+                return new List<BrandResponse>();
+            }
+
+            return brands.Adapt<List<BrandResponse>>();
+        }
+    }
+}
---
+++ ESFE.BusinessLogic/UsesCases/Brands/Queries/GetBrands/GetBrandsQuery.cs
@@ -0,0 +1,11 @@
+﻿using ESFE.BusinessLogic.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE.BusinessLogic.UsesCases.Brands.Queries.GetBrands
+
+    public record GetBrandsQuery() : IRequest<List<BrandResponse>>;
