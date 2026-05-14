f241ee0a57a30f74de39d0f3cf6e5e5b06c2d90d
abigail23181 <abigailrodrigxz@gmail.com>
2026-05-13 19:03:12 -0600

    Reina Subio Cambios.

---
+++ ESFE.BusinessLogic/UsesCases/Products/Queries/GetProducts/GetProdctosHandler.cs
@@ -0,0 +1,23 @@
+﻿using ESFE.BusinessLogic.DTOs;
+using ESFE.DataAccess.Interfaces;
+using ESFE.Entities;
+using Mapster;
+using MediatR;
+
+namespace ESFE.BusinessLogic.UseCases.Products.Queries.GetProducts
+{
+    internal sealed class GetProductsHandler(IEfRepository<Product> _repository) : IRequestHandler<GetProductsQuery, List<ProductResponse>>
+    {
+        public async Task<List<ProductResponse>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {
+            var products = await _repository.ListAsync(new GetProductWithBrandSpec(), cancellationToken);
+
+            if (products == null && !products.Any())
+            {
+                return new List<ProductResponse>();
+            }
+
+            return products.Adapt<List<ProductResponse>>();
+        }
+    }
+}
